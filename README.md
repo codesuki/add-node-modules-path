9ed240e05dcb9628ba380151b54b02688be5e78e
codesuki <marschik_neri@cyberagent.co.jp>
2016-09-12 18:20:15 +0900

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # add-node-modules-path
-Adds the node_modules/.bin directory to the buffer exec_path. E.g. support project local eslint installations.
+This file provides `add-node-modules-path`, which searches
+the current files parent directories for the `node_modules/.bin/` directory
+and adds it to the buffer local `exec-path`.
+This allows Emacs to find project based installs of e.g. eslint.
+
+## Usage
+`M-x add-node-modules-path`
+
+To automatically run it when opening a new buffer:
+(Choose depending on your favorite mode.)
+
+```
+(eval-after-load 'js-mode
+  '(add-hook 'js-mode-hook #'add-node-modules-path))
+
+(eval-after-load 'js2-mode
+  '(add-hook 'js2-mode-hook #'add-node-modules-path))
+```
---
+++ add-node-modules-path.el
@@ -0,0 +1,50 @@
+;;; add-node-modules-path.el --- Add node_modules to your exec-path
+
+;; Copyright (C) 2016 Neri Marschik
+;; This package uses the MIT License.
+;; See the LICENSE file.
+
+;; Author: Neri Marschik <marschik_neri@cyberagent.co.jp>
+;; Version: 1.0
+;; Package-Requires: ()
+;; Keywords: javascript, node, node_modules, eslint
+;; URL: https://github.com/codesuki/add-node-modules-path
+
+;;; Commentary:
+;;
+;; This file provides `add-node-modules-path', which searches
+;; the current files parent directories for the `node_modules/.bin/' directory
+;; and adds it to the buffer local `exec-path'.
+;; This allows Emacs to find project based installs of e.g. eslint.
+;;
+;; Usage:
+;;     M-x add-node-modules-path
+;;
+;;     To automatically run it when opening a new buffer:
+;;     (Choose depending on your favorite mode.)
+;;
+;;     (eval-after-load 'js-mode
+;;       '(add-hook 'js-mode-hook #'add-node-modules-path))
+;;
+;;     (eval-after-load 'js2-mode
+;;       '(add-hook 'js2-mode-hook #'add-node-modules-path))
+
+;;; Code:
+
+;;;###autoload
+(defun add-node-modules-path ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (path (and root
+                    (expand-file-name "node_modules/.bin/" root))))
+    (if root
+        (progn
+          (make-local-variable 'exec-path)
+          (add-to-list 'exec-path path)
+          (message "added node_modules to exec-path"))
+      (message "node_modules not found"))))
+
+(provide 'add-node-modules-path)
+
+;;; add-node-modules-path.el ends here
