4b8cc500c0663842ece9a6fcad48482e09d8d71f
kayhide <kayhide@gmail.com>
2019-10-27 19:36:54 +0900

    Use npm command to find bin path

--- add-node-modules-path.el
+++ add-node-modules-path.el
@@ -12,9 +12,8 @@
 
 ;;; Commentary:
 ;;
-;; This file provides `add-node-modules-path', which searches
-;; the current files parent directories for the `node_modules/.bin/' directory
-;; and adds it to the buffer local `exec-path'.
+;; This file provides `add-node-modules-path', which runs `npm bin` and
+;; and adds the path to the buffer local `exec-path'.
 ;; This allows Emacs to find project based installs of e.g. eslint.
 ;;
 ;; Usage:
@@ -32,46 +31,37 @@
 ;;; Code:
 
 ;;;###autoload
+(defcustom add-node-modules-path-command "npm bin"
+  "Command to find the bin path."
+  :type 'string)
+
+;;;###autoload
 (defcustom add-node-modules-path-debug nil
   "Enable verbose output when non nil."
   :type 'boolean)
 
 ;;;###autoload
-(defcustom add-node-modules-max-depth 20
-  "Max depth to look for node_modules."
-  :type 'integer)
-
-;;;###autoload
 (defun add-node-modules-path ()
-  "Search the current buffer's parent directories for `node_modules/.bin`.
-Traverse the directory structure up, until reaching the user's home directory,
- or hitting add-node-modules-max-depth.
-Any path found is added to the `exec-path'."
+  "Run `npm bin` command and add the path to the `exec-path`.
+If `npm` command fails, it does nothing."
   (interactive)
-  (let* ((default-dir (expand-file-name default-directory))
-         (file (or (buffer-file-name) default-dir))
-         (home (expand-file-name "~"))
-         (iterations add-node-modules-max-depth)
-         (root (directory-file-name (or (and (buffer-file-name) (file-name-directory (buffer-file-name))) default-dir)))
-         (roots '()))
-    (while (and root (> iterations 0))
-      (setq iterations (1- iterations))
-      (let ((bindir (expand-file-name "node_modules/.bin/" root)))
-        (when (file-directory-p bindir)
-          (add-to-list 'roots bindir)))
-      (if (string= root home)
-          (setq root nil)
-        (setq root (directory-file-name (file-name-directory root)))))
-    (if roots
-        (progn
-          (make-local-variable 'exec-path)
-          (while roots
-            (add-to-list 'exec-path (car roots))
-            (when add-node-modules-path-debug
-              (message (concat "added " (car roots) " to exec-path")))
-            (setq roots (cdr roots))))
+
+  (let* ((res (s-chomp (shell-command-to-string add-node-modules-path-command)))
+         (exists (file-exists-p res))
+         )
+    (cond
+     (exists
+      (make-local-variable 'exec-path)
+      (add-to-list 'exec-path res)
       (when add-node-modules-path-debug
-        (message (concat "node_modules/.bin not found for " file))))))
+        (message "Added to `exec-path`: %s" res))
+      )
+     (t
+      (when add-node-modules-path-debug
+        (message "Failed to run `%s':\n %s" add-node-modules-path-command res))
+      ))
+    )
+  )
 
 (provide 'add-node-modules-path)
 
